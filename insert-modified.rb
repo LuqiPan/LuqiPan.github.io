f2481a6cb66a97c7f8eae76100bef6960cda52d6
Luqi Pan <luqi.pan@gmail.com>
2014-01-10 13:45:16 -0800

    Getting close to insert-modified done.

--- insert-modified.rb
+++ insert-modified.rb
@@ -1,6 +1,20 @@
+def insert_modified(filename)
+    File.open(filename, 'r') do |fin|
+    File.open("./tmp", 'w') do |fout|
+        while line = fin.gets
+            fout.puts line unless line.start_with?("modified: \"")
+            if line.start_with?("tags: [")
+                fout.puts ("modified: \"" + File.mtime(filename).strftime("%Y-%m-%d %H:%M %Z\""))
+            end
+        end
+    end
+    end
+end
+
 Dir.glob("_posts/**/*") { |filename| 
     if filename[-3..-1] == ".md"
         puts filename
-        puts File.mtime("filename")
+        insert_modified(filename)
+        puts
     end
 }
