cc9706eca169383a955382359b4103259c81f224
Luqi Pan <luqi.pan@gmail.com>
2014-01-11 18:17:37 -0500

    Finished the imagined insert-modified.rb(But it's not what I want) Gonna keep it and think about a different way to accomplish this feature.

--- insert-modified.rb
+++ insert-modified.rb
@@ -1,14 +1,17 @@
+require 'fileutils'
+
 def insert_modified(filename)
     File.open(filename, 'r') do |fin|
     File.open("./tmp", 'w') do |fout|
         while line = fin.gets
             fout.puts line unless line.start_with?("modified: \"")
             if line.start_with?("tags: [")
-                fout.puts ("modified: \"" + File.mtime(filename).strftime("%Y-%m-%d %H:%M %Z\""))
+                #fout.puts ("modified: \"" + File.mtime(filename).strftime("%A, %d %b %Y, %H:%M\""))
             end
         end
     end
     end
+    FileUtils.mv("./tmp", filename)
 end
 
 Dir.glob("_posts/**/*") { |filename| 
